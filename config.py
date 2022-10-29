bcecf52c64a5ed496ea2ce364aca4faee11cdc4b
Dustin Jensen <110437485+dustyj1984@users.noreply.github.com>
2022-10-29 11:49:01 -0700

    class2

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
+import pymongo
+import certifi
+
+con_str = "mongodb+srv://dustyj1984:Team2022@cluster0.qblxpsa.mongodb.net/?retryWrites=true&w=majority"
+
+client = pymongo.MongoClient(con_str, tlsCAFile=certifi.where())
+
+db = client.get_database("softx")
+
 
 me = {
     "first_name": "Dustin",
--- server.py
+++ server.py
@@ -1,23 +1,14 @@
-from unicodedata import category
-from flask import Flask
+
+import re
+from colorama import Cursor
+from flask import Flask, request, abort 
 import json 
-from config import me, hello
+from config import me, db
 from mock_data import catalog
+from bson import ObjectId
+
 
 app = Flask('Server')
-
-
-@app.get('/')
-def home():
-    return "Hello from Flask!"
-
-@app.get('/test')
-def test():
-    return "this test"
-
-@app.get('/about')
-def about():
-    return "Dustin Jensen"
 
 
 ################################################################
@@ -33,74 +24,110 @@ def version():
         "name": "sloth",
         "developer": me 
     }
-    hello()
     return json.dumps(v)
+
+def fix_id(obj):
+    obj['_id'] = str(obj['_id'])
+    return obj
+
 
 @app.get('/api/catalog')
 def get_catalog():
-    return json.dumps( len(catalog) )
+    cursor = db.products.find({}).sort("title")
+    results = []
+    for prod in cursor:
+        results.append(fix_id(prod))
+    return json.dumps(results)
+
+
+@app.post('/api/catalog')
+def save_product():
+    product= request.get_json()
+
+    if product is None:
+        return abort(400, "No Product required")
+
+    product["category"] = product["category"].lower()
+
+    db.products.insert_one(product)
+    product["_id"] = str(product["_id"])
+    return json.dumps(product)
+
+@app.put("/api/catalog")
+def update_product():
+    product = request.get_json()
+    id = product.pop("_id") # read and remove the id from the product
+
+    db.products.update_one({"_id": ObjectId(id)}, {"$set":product})
+    return json.dumps('ok')
+
+@app.delete("/api/catalog/<id>")
+def delete_product(id):
+    res = db.products.delete_one({"_id": ObjectId(id)})
+    return json.dumps({"count": res.deleted_count})
+
 
     
+
 @app.get('/api/products/count')
 def total_count():
-    return json.dumps( len(catalog) )
+    count = db.products.count_documents({})
+    return json.dumps(count)
+
 
 @app.get('/api/products/total')
 def total_price():
     total = 0
-    for prod in catalog:
+    cursor = db.products.find({})
+    for prod in cursor:
         total += prod['price']
-    
-    return json.dumps( total )
+    return json.dumps(total)
+
+@app.get('/api/products/details/<id>')
+def product_details(id):
+    prod = db.products.find_one({"_id": ObjectId(id)})
+    if prod:
+        return json.dumps(fix_id(prod))
+    return abort(404, "Product not found")
+
+
+
+   
 
 @app.get('/api/catalog/<category>')
 def by_category(category):
 
     results = []
-    for prod in catalog:
-        if prod['category'].lower == category.lower():
-            results.append( prod )
+    cursor = db.products.find({"category": category})
+    for prod in cursor:
+        results.append(fix_id(prod))
     return json.dumps( results )
+
 
 @app.get('/api/catalog/lower/<amount>')
 def lower_than(amount):
     results = []
-    for prod in catalog:
-        if prod['price'] < float(amount):
-            results.append( prod )
+    cursor = db.products.find({"price": {"$lt": float(amount)}})
+    for prod in cursor:
+        results.append(fix_id(prod))
+    return json.dumps(results)
 
+@app.get('/api/catalog/higher/<amount>')
+def higher_than(amount):
+    results = []
+    cursor = db.products.find({"price": {"$gte": float(amount)}})
+    for prod in cursor:
+        results.append(fix_id(prod))
     return json.dumps(results)
     
 @app.get('/api/categroy/unique')
 def unique_cats():
     results = []
-    for prod in catalog:
-        category = prod['category']
-        if not category in results:
-            results.append(category)
-
+    cursor = db.products.distinct("category")
+    for cat in cursor:
+        results.append(cat)
     return json.dumps( results )
 
-@app.get('/api/test/colors')
-def unique_colors():
-    colors = ["red", 'blue',"Pink", "yelloW", "Red", "Black", "BLUE", "RED", "BLACK", "YELLOW"]
-    results = []
-    for color in colors:
-        color = color.lower()
-        if color not in results:
-            results.append(color)
 
-    return json.dumps(results)
 
-@app.get('/api/test/count/<color>')
-def count_color(color):
-    color = color.lower()
-    colors = ["red", 'blue',"Pink", "yelloW", "Red", "Black", "BLUE", "RED", "BLACK", "YELLOW"]
-    count = 0
-    for item in colors:
-        if color == item.lower():
-            count += 1
-
-    return json.dumps( count )
-
-app.run(debug=True)
+#app.run(debug=True)
